4167bdd7663e5dcf071b53310f798d4c20710a44
paucabral <josepaucabral@gmail.com>
2021-12-23 22:48:35 +0800

    Added copy to clipboard button for HTTPS

--- static/js/script.js
+++ static/js/script.js
@@ -87,3 +87,15 @@ function showFileNameSecret(event) {
   var fileName = input.files[0].name;
   infoArea.textContent = 'File name: ' + fileName;
 }
+
+function copyClip() {
+  var copyText = document.getElementById("exampleFormControlTextarea1");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  // /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  alert("Copied!")
+}
