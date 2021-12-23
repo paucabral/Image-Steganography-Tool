ccf844c31eb902c2a5854a896cd32e879b843a4d
paucabral <josepaucabral@gmail.com>
2021-12-23 20:06:57 +0800

    Auto Pep-8 formatted

--- app.py
+++ app.py
@@ -4,53 +4,68 @@ import txt2img
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/text-to-image", methods=['GET'])
 def text2imageIndex():
-  return render_template('text-to-image-index.html')
+    return render_template('text-to-image-index.html')
+
 
 @app.route("/text-to-image/encode", methods=['GET', 'POST'])
 def text2imageEncode():
-  if request.method == 'GET':
-    return render_template('text-to-image-encode.html')
+    if request.method == 'GET':
+        return render_template('text-to-image-encode.html')
+    if request.method == 'POST':
+
+        return render_template('text-to-image-encode.html')
+
 
 @app.route("/text-to-image/encode/result", methods=['GET'])
 def text2imageEncodeResult():
-  return render_template('text-to-image-encode-result.html')
+    return render_template('text-to-image-encode-result.html')
+
 
 @app.route("/text-to-image/decode", methods=['GET', 'POST'])
 def text2imageDecode():
-  if request.method == 'GET':
-    return render_template('text-to-image-decode.html')
+    if request.method == 'GET':
+        return render_template('text-to-image-decode.html')
+
 
 @app.route("/text-to-image/decode/result", methods=['GET'])
 def text2imageDecodeResult():
-  return render_template('text-to-image-decode-result.html')
+    return render_template('text-to-image-decode-result.html')
+
 
 @app.route("/image-to-image", methods=['GET'])
 def image2imageIndex():
-  return render_template('image-to-image-index.html')
+    return render_template('image-to-image-index.html')
+
 
 @app.route("/image-to-image/encode", methods=['GET', 'POST'])
 def image2imageEncode():
-  if request.method == 'GET':
-    return render_template('image-to-image-encode.html')
+    if request.method == 'GET':
+        return render_template('image-to-image-encode.html')
+
 
 @app.route("/image-to-image/encode/result", methods=['GET'])
 def image2imageEncodeResult():
-  return render_template('image-to-image-encode-result.html')
+    return render_template('image-to-image-encode-result.html')
+
 
 @app.route("/image-to-image/decode", methods=['GET', 'POST'])
 def image2imageDecode():
-  if request.method == 'GET':
-    return render_template('image-to-image-decode.html')
+    if request.method == 'GET':
+        return render_template('image-to-image-decode.html')
+
 
 @app.route("/image-to-image/decode/result", methods=['GET'])
 def image2imageDecodeResult():
-  return render_template('image-to-image-decode-result.html')
+    return render_template('image-to-image-decode-result.html')
+
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
