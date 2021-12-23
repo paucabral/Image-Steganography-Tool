bee43bded73b43bf2b3c9a6b86c13172f15596e8
paucabral <josepaucabral@gmail.com>
2021-12-23 19:39:56 +0800

    Made image upload inputs.

--- static/js/script.js
+++ static/js/script.js
@@ -19,6 +19,42 @@ $(function () {
   });
 });
 
+function readURLCover(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#imageResultCover')
+        .attr('src', e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(function () {
+  $('#uploadCover').on('change', function () {
+    readURLCover(input);
+  });
+});
+
+function readURLSecret(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#imageResultSecret')
+        .attr('src', e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(function () {
+  $('#uploadSecret').on('change', function () {
+    readURLSecret(input);
+  });
+});
+
 /*  ==========================================
     SHOW UPLOADED IMAGE NAME
 * ========================================== */
@@ -27,6 +63,26 @@ var infoArea = document.getElementById('upload-label');
 
 input.addEventListener('change', showFileName);
 function showFileName(event) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
+
+var inputCover = document.getElementById('uploadCover');
+var infoAreaCover = document.getElementById('upload-labelCover');
+
+inputCover.addEventListener('change', showFileNameCover);
+function showFileNameCover(event) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
+
+var inputSecret = document.getElementById('uploadSecret');
+var infoAreaSecret = document.getElementById('upload-labelSecret');
+
+inputSecret.addEventListener('change', showFileNameSecret);
+function showFileNameSecret(event) {
   var input = event.srcElement;
   var fileName = input.files[0].name;
   infoArea.textContent = 'File name: ' + fileName;
